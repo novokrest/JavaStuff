038b8e2ba938305e5bdea914869ed834c622a3a9
novokrest <novokrest013@gmail.com>
2016-06-19 12:41:45 +0300

    DoubleChecking: synchronized on inner static field instead of .class

--- Singleton/src/com/_1usualpro/javastuff/singletons/lazy/DoubleCheckingSingleton.java
+++ Singleton/src/com/_1usualpro/javastuff/singletons/lazy/DoubleCheckingSingleton.java
@@ -6,12 +6,13 @@ package com._1usualpro.javastuff.singletons.lazy;
  * - only with JavaSDK 1.5 and higher
  */
 public class DoubleCheckingSingleton {
+    private static final Object sLock = new Object();
     private static volatile DoubleCheckingSingleton sInstance;
 
     private static DoubleCheckingSingleton getInstance() {
         DoubleCheckingSingleton localInstance = sInstance;
         if (localInstance == null) {
-            synchronized (DoubleCheckingSingleton.class) {
+            synchronized (sLock) {
                 localInstance = sInstance;
                 if (localInstance == null) {
                     sInstance = localInstance = new DoubleCheckingSingleton();
